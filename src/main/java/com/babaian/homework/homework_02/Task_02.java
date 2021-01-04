c333e8a1f12a8c1f907082c33e5741b855b35d6d
Ukrnox <ukr.nox@gmail.com>
2021-01-05 01:30:11 +0200

    edited Task_02 & Task_03 (Homework_02)

--- src/main/java/com/babaian/homework/homework_02/Task_02.java
+++ src/main/java/com/babaian/homework/homework_02/Task_02.java
@@ -2,12 +2,12 @@ package com.babaian.homework.homework_02;
 
 public class Task_02 {
 
-    public static double Heron (double a,double b, double c){
+    public static double heron (double a,double b, double c){
         double p = (a+b+c)/2;
         return Math.sqrt(p*(p-a)*(p-b)*(p-c));
 
     }
     public static void main(String[] args) {
-        System.out.println(Heron(3,4,5));
+        System.out.println(heron(3,4,5));
     }
 }
--- src/main/java/com/babaian/homework/homework_02/Task_03.java
+++ src/main/java/com/babaian/homework/homework_02/Task_03.java
@@ -4,8 +4,8 @@ public class Task_03 {
 
     public static void sq (double a,double b, double c) {
         double[] result = new double[2];
-        result[0] = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / 2 * a;
-        result[1] = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / 2 * a;
+        result[0] = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+        result[1] = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
         if (result[0] > result[1]) {
             double temp = result[0];
             result[0] = result[1];
