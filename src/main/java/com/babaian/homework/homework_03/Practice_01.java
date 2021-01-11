d5c695e309ab3015ce11ffc2daf6b9e2ce7f1983
Ukrnox <ukr.nox@gmail.com>
2021-01-11 23:38:58 +0200

    Add HW3 Practice_01

---
+++ src/main/java/com/babaian/homework/homework_03/Practice_01.java
@@ -0,0 +1,11 @@
+package com.babaian.homework.homework_03;
+
+public class Practice_01 {
+
+    public static int stayHydrated(double time){
+        return (int) (time * 0.5);
+    }
+    public static void main(String[] args) {
+        System.out.println(stayHydrated(6.7));
+    }
+}
