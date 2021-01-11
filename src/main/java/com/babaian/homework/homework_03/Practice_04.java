68f637622e85c017ac8bbfc2c00203eb73096fc6
Ukrnox <ukr.nox@gmail.com>
2021-01-11 23:39:57 +0200

    Add HW3 Practice_04

---
+++ src/main/java/com/babaian/homework/homework_03/Practice_04.java
@@ -0,0 +1,16 @@
+package com.babaian.homework.homework_03;
+
+public class Practice_04 {
+
+    public static String describeAge(int age) {
+        return "You're a(n) " + ((age <= 12) ? "kid."
+                : (age <= 17) ? "teenager."
+                : (age <= 64) ? "adult."
+                : "elderly.");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(describeAge(13));
+    }
+
+}
