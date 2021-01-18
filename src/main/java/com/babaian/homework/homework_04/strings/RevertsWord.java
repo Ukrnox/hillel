f19602f6c66f72b3ec129600f44cfeacc13f3e26
Ukrnox <ukr.nox@gmail.com>
2021-01-19 00:46:31 +0200

    Add class RevertsWord

---
+++ src/main/java/com/babaian/homework/homework_04/strings/RevertsWord.java
@@ -0,0 +1,28 @@
+package com.babaian.homework.homework_04.strings;
+
+public class RevertsWord {
+    public static String revertWordSB(String word) {
+        StringBuilder result = new StringBuilder(word);
+        result.reverse();
+        return result.toString();
+    }
+
+    private static String revertWordCA(String word) {
+        char[] array = word.toCharArray();
+        int left = 0;
+        char temp;
+        int right = array.length - 1;
+        while (left < right) {
+            temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+        return String.valueOf(array);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(revertWordSB("Hello!"));
+    }
+}
