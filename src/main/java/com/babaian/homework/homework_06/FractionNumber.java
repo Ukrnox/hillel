e3c9a6a745c9130f062c85b6114426d7f8dd8e83
Ukrnox <ukr.nox@gmail.com>
2021-01-22 13:55:22 +0200

    Add class FractionNumber

---
+++ src/main/java/com/babaian/homework/homework_06/FractionNumber.java
@@ -0,0 +1,83 @@
+package com.babaian.homework.homework_06;
+
+import java.util.Objects;
+
+public class FractionNumber implements Fraction <FractionNumber> {
+
+    private final int numerator;
+    private final int denominator;
+
+    public FractionNumber(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new IllegalArgumentException("Zero in the denominator!\u2639");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+
+    }
+
+    @Override
+    public int getNumerator() {
+        return numerator;
+    }
+
+    @Override
+    public int getDenominator() {
+        return denominator;
+    }
+
+    @Override
+    public FractionNumber plus(FractionNumber a) {
+        int comDenominator = comDenominator(this.denominator, a.getDenominator());
+        return new FractionNumber(((this.numerator * (comDenominator / this.denominator)) +
+                (a.getNumerator() * comDenominator / a.getDenominator())), (comDenominator));
+    }
+
+    @Override
+    public FractionNumber minus(FractionNumber a) {
+        int comDenominator = comDenominator(this.denominator, a.getDenominator());
+        return new FractionNumber(((this.numerator * (comDenominator / this.denominator)) -
+                (a.getNumerator() * comDenominator / a.getDenominator())), (comDenominator));
+    }
+
+    @Override
+    public FractionNumber multiply(FractionNumber a) {
+        return new FractionNumber((this.numerator * a.getNumerator()), this.denominator * a.getDenominator());
+    }
+
+    @Override
+    public FractionNumber divide(FractionNumber a) {
+        return this.multiply(new FractionNumber(a.getDenominator(), a.getNumerator()));
+    }
+
+    public String printToMixedFractions() {// добавил возможность вывода в консоль целого если оно есть ex: "-3 and 9/127"
+        int whole = numerator / denominator;
+        if (whole == 0) {
+            return toString() + " is not a mixed fraction";
+        }
+        int comDivisor = comDivisor(numerator, denominator);
+        int newDenominator = Math.abs(denominator / comDivisor);
+        int newNumerator = Math.abs(numerator / comDivisor) % newDenominator;
+        return toString() + " can be a mixed fraction: " + whole + " and " + newNumerator + "/" + newDenominator;
+    }
+
+    @Override
+    public String toString() {
+        return numerator / comDivisor(numerator, denominator) +
+                "/" + denominator / comDivisor(numerator, denominator);
+    }
+
+    @Override
+    public boolean equals(Object a) {
+        if (this == a) return true;
+        if (a == null || getClass() != a.getClass()) return false;
+        FractionNumber that = (FractionNumber) a;
+        return numerator == that.numerator && denominator == that.denominator;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numerator, denominator);
+    }
+
+}
