daf39a1b210e4d94d16b8640ab41c44c83fac951
Ukrnox <ukr.nox@gmail.com>
2021-02-23 15:19:36 +0200

    Add class DataMapper

---
+++ src/main/java/com/babayan/homeworks/homework_14/DataMapper.java
@@ -0,0 +1,18 @@
+package com.babayan.homeworks.homework_14;
+
+public class DataMapper {
+
+    public static Person apply(String s) {
+        try {
+            String[] line = s.split(",");
+            return new Person(line[0], line[1],
+                    Integer.valueOf(line[2]),
+                    Integer.valueOf(line[3]),
+                    Integer.valueOf(line[4]),
+                    line[5]);
+        } catch (NullPointerException | NumberFormatException | IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
