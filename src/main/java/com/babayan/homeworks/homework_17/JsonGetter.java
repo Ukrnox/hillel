3268f5c479e562c93899581aea9e8991b71bb1f9
Ukrnox <ukr.nox@gmail.com>
2021-03-07 08:16:21 +0200

    Edited class JsonGetter

--- src/main/java/com/babayan/homeworks/homework_17/JsonGetter.java
+++ src/main/java/com/babayan/homeworks/homework_17/JsonGetter.java
@@ -5,23 +5,31 @@ import java.net.Socket;
 import java.net.SocketException;
 
 public class JsonGetter {
-    public static String getJsonString() throws IOException {
-        Socket socket = new Socket("jsonplaceholder.typicode.com", 80);
-        InputStream inputStream = socket.getInputStream();
-        String s = "GET /users/5 HTTP/1.1\n" +
-                "Host: jsonplaceholder.typicode.com\n" +
-                "\n" +
-                "\n";
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(s.getBytes());
-        int ch;
-        StringBuilder response = new StringBuilder();
-        try {
-            while ((ch = inputStream.read()) != -1) {
-                response.append((char) ch);
+    public static String getJsonString() {
+        StringBuilder response;
+        try(Socket socket = new Socket("jsonplaceholder.typicode.com", 80);
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream()){
+            String s = "GET /users/5 HTTP/1.1\n" +
+                    "Host: jsonplaceholder.typicode.com\n" +
+                    "\n" +
+                    "\n";
+            outputStream.write(s.getBytes());
+            response = new StringBuilder();
+            System.out.println(socket.isConnected());
+            try {
+                int ch;
+                while (socket.isConnected()) {
+                    ch = inputStream.read();
+                    System.out.print((char) ch);
+                    response.append((char) ch);
+                }
+            } catch (SocketException e) {
+                e.printStackTrace();
             }
-        } catch (SocketException e) {
+        }catch (IOException e){
             e.printStackTrace();
+            return null;
         }
         String[] split = response.toString().split("\n(?=\\{)");
         return split[1];
